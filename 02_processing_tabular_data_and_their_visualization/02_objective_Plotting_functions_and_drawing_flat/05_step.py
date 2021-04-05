8163be875d48ce386356ccd24b3f3f6ad1664f97
KorkhovPavel <korkhov.pi@gmail.com>
2021-04-05 16:29:09 +0300

    done: 02_processing_tabular_data_and_their_visualization 02_objective_Plotting_functions_and_drawing_flat 05_step.py

---
+++ 02_processing_tabular_data_and_their_visualization/02_objective_Plotting_functions_and_drawing_flat/05_step.py
@@ -0,0 +1,38 @@
+# Дан фрагмент программного кода:
+#
+# import matplotlib.pyplot as plt
+#
+# x_list = [1, 5, -3]
+#
+# y_list = [-5, 6, 1 ]
+#
+# #функция создания линии
+#
+# plt.show()
+# Сопоставьте функцию, которую нужно вставить в программу вместо комментария, и результат.
+
+import matplotlib.pyplot as plt
+
+x_list = [1, 5, -3]
+
+y_list = [-5, 6, 1]
+
+# 1
+# plt.plot(x_list, y_list, "r--")
+# plt.show()
+
+# 2
+# plt.plot(x_list, y_list, "r+-")
+# plt.show()
+
+# 3
+# plt.plot(y_list, "gv")
+# plt.show()
+
+# 4
+# plt.plot(x_list, y_list, "gv")
+# plt.show()
+
+# 5
+# plt.plot(x_list, x_list)
+# plt.show()
