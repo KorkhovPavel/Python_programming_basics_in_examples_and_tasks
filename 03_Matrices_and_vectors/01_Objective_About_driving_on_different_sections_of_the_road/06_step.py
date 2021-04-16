abbc42aee6efab0736f11261402a9ad5599cccc0
KorkhovPavel <korkhov.pi@gmail.com>
2021-04-16 17:12:10 +0300

    done: 03_Matrices_and_vectors 01_Objective_About_driving_on_different_sections_of_the_road 06_step.py 08_step.py

---
+++ 03_Matrices_and_vectors/01_Objective_About_driving_on_different_sections_of_the_road/06_step.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([2, 5, -2, 0, 8])
+
+b = np.array([5, -1, -3, 6, 2])
+
+print(a + b)
+
+print(np.sum(np.cos(b)))
+
+print(np.sum(a) + np.mean(a))
+
+print(np.round(a, 2))
+
+print(a + np.sum(b))
+
+print(np.pi - a)
