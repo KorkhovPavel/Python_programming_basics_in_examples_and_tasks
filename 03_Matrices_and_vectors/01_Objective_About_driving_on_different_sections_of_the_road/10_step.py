0f7071a6d7851b7474dc0d191088262cb855cd5c
KorkhovPavel <korkhov.pi@gmail.com>
2021-04-16 19:53:50 +0300

    done: 03_Matrices_and_vectors 01_Objective_About_driving_on_different_sections_of_the_road 10_step.py 11_step.py 13_step.py

---
+++ 03_Matrices_and_vectors/01_Objective_About_driving_on_different_sections_of_the_road/10_step.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([2, 5, 2, 0, 0])
+
+b = np.array([5, -1, -1, 6, 2])
+
+
+# c = np.where(a == b)[0]
+# print(c)
+
+# c = np.where(np.logical_and(a > 1, b < 0))[0]
+# print(c)
+# c = np.where(np.logical_or(a >= 0, b == 6))[0]
+# print(c)
+# c = np.where(np.logical_not(b <= 0))[0]
+# print(c)
+# c = np.where(a == 0)[0]
+# print(c)
