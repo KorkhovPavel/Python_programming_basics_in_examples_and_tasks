a946156f615f4f420b2576c6d0cfd8788138f079
korkhovpavel <korkhov.pi@gmail.com>
2021-04-30 20:11:43 +0300

    done: 03_Matrices_and_vectors 03_A_task_Matrix_operations 05_step.py 07_step.py

---
+++ 03_Matrices_and_vectors/03_A_task_Matrix_operations/05_step.py
@@ -0,0 +1,26 @@
+import numpy as np
+import numpy.linalg as alg
+
+a = np.array([[3, 4, -2], [-2, -1, 4]])
+
+b = np.array([[1, -3, -2, 1], [2, 4, -2, -1], [5, -2, 0, -2]])
+
+c = np.array([[-1, 0, 2], [2, -2, 3], [0, 5, 1]])
+
+# det_array = np.det(c)
+# print(det_array)
+
+# det_array = np.det(a)
+# print(det_array)
+
+# d = 5 * a
+# print(d)
+
+# d = 12 - b
+# print(d)
+
+# det_array = alg.det(b)
+# print(det_array)
+
+# d = b - c
+# print(d)
